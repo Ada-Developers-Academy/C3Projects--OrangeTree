c572d84408154e1a7cf34c233bb9f55cbbcec3ca
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-05-17 23:22:57 -0700

    changed plant_trees input validation to exclude floats

--- orange_tree.rb
+++ orange_tree.rb
@@ -11,8 +11,10 @@ class OrangeTree
 		@orange_count = 0
 	end
 
+	# start growing oranges at age 3 years
 	def grow_oranges
 		if @age >= 3
+			# number of oranges grown depends on soil_quality
 			if @grove.soil_quality == "unhealthy"
 				@orange_count = @age
 			elsif @grove.soil_quality == "average"
@@ -23,6 +25,7 @@ class OrangeTree
 		end
 	end
 
+	# trees grow 2 feet per year up til age 5, at which point they grow 0.1 feet / year
 	def grow_taller
 		if @age < 5
 			@height += 2
@@ -39,6 +42,7 @@ class OrangeTree
 		puts "This tree has died."
 	end
 
+	# the soil quality of the grove affects when a tree dies
 	def grow_older
 		if @grove
 			if @grove.soil_quality == "unhealthy" && @age >= 5
@@ -48,6 +52,7 @@ class OrangeTree
 			end
 		end
 
+		# if a tree isn't dead, increment the age by 1
 		if age >= 10
 			kill_tree
 		else
@@ -56,6 +61,7 @@ class OrangeTree
 
 	end
 
+	# when a year passes, the tree grows older, gets taller, and grows oranges
 	def one_year_passes
 		grow_older
 		if @alive == true
@@ -63,11 +69,13 @@ class OrangeTree
 			grow_oranges
 		end
 
+		# if the tree is in a grove, that grove's soil quality is updated
 		if @grove
 			@grove.soil_quality
 		end
 	end
 
+	# pick orange or tell user there are no oranges
 	def pick_orange
 		if @orange_count != 0
 			@orange_count -= 1
@@ -78,6 +86,7 @@ class OrangeTree
 		end
 	end
 
+	# add tree to grove
 	def plant_on(grove_name)
 		grove_name.trees.push(self)
 		@grove = grove_name
@@ -91,11 +100,11 @@ class OrangeGrove
 	def initialize
 		@trees = []
 		@total_orange_count = 0
-		@soil_quality = "healthy"
+		@soil_quality = "healthy"	# start soil_quality at healthy level
 	end
 
 	def soil_quality
-		# make it based on if trees are alive
+		# returns soil_quality based on how many trees are in the grove
 		if @trees.length <= 5
 			@soil_quality = "healthy"
 		elsif @trees.length <=10
@@ -106,8 +115,10 @@ class OrangeGrove
 		return @soil_quality
 	end
 
+	# add trees to a grove
 	def plant_trees(number)
-		if number.to_i != 0
+		# check the input to make sure it's an integer
+		if number.to_i != 0 && !(number.to_s.include?("."))
 			number.times do
 				new_tree = OrangeTree.new
 				new_tree.plant_on(self)
@@ -118,13 +129,14 @@ class OrangeGrove
 		end
 	end
 
+	# one year passes for the entire grove
 	def one_year_passes_grove
 		@total_orange_count = 0
 		@trees.each do |tree|
 			tree.one_year_passes
 			@total_orange_count += tree.orange_count
 		end
-		soil_quality
+		# soil_quality
 	end
 
 end
