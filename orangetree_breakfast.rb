37dbd7592fb10ab9e33e8ba818f415fb1977d5f2
Loren Wang <wang131.131@gmail.com>
2015-06-11 12:36:01 -0700

    completed Lunch level

--- orangetree_breakfast.rb
+++ orangetree_breakfast.rb
@@ -1,7 +1,7 @@
 class OrangeTree #
-  attr_reader :age, :is_dead, :max_age, :height, :count
+  attr_reader :age, :height, :orange_count, :max_age, :is_dead
   def initialize
-    @age = 0 # starting our age, height, and count at 0 when it first starts out
+    @age = 0
     @height = 0
     @orange_count = 0
     @max_age = 10
@@ -9,25 +9,28 @@ class OrangeTree #
   end
 
   def one_year_passes
-    @age += 1 #increments the orange tree's age by one year
-    @height += 4 # increments the tree's height by 4 units
+    @age += 1
+    @height += 4
     @orange_count = 0 # after every year, the count of oranges goes back to 0
-    if age > max_age # when it surpasses being 10 years old, we make it die
+    if age > max_age
       @is_dead = true
       puts "Your tree is dead."
-    else # when the orange tree is 3 years old, it grows oranges
-      grow_oranges #calling grow_oranges grows and counts the number of oranges
+    else
+      grow_oranges
     end
   end
 
   def grow_oranges # method to grow & count the oranges on the tree
     if @age <= 6 && @age >= 3 # between the age of 3-6 years, it will grow a random amount of oranges
-        @orange_count=rand(50..100)
+      @orange_count=rand(50..100)
     elsif @age <= @max_age && @age > 6 # from age 7-10, it will grow a higher # of oranges
       @orange_count=rand(100..300)
     end
   end
 
+  def orange_count
+    return @orange_count
+  end
 
   def pick_orange
     if @orange_count > 0 #if there are oranges to be picked, and we use the pick_orange method,
@@ -40,28 +43,29 @@ class OrangeTree #
 end
 
 class OrangeGrove
-  attr_reader :trees, :count, :age
+  attr_reader :trees, :orange_count, :age
+
   def initialize(trees)
-    tree_array = []  # creating an array to hold each new tree
-    (1..trees).each do |initialize| # runs through the block as many times as specified
+    tree_array = []
+    (1..trees).each do  # runs through the block as many times as specified
       tree_instance = OrangeTree.new    # creates new tree
-      tree_array.push(tree_instance)       # places new tree into the array
+      tree_array.push(tree_instance)    # places new tree into the array
     end
     puts tree_array
     @trees = tree_array
     @orange_count = 0
   end
 
-  def count_all_oranges
-    @trees.each do |t|
-      @orange_count += rand(50..100) # random number oranges per tree
-  end
-  puts @orange_count
-  end
-
   def grow_grove
     @trees.each do |y|
       y.one_year_passes
     end
+  end
+
+  def count_all_oranges
+    @trees.each do |x|
+      @orange_count += x.orange_count
+    end
+    @orange_count
   end
 end
