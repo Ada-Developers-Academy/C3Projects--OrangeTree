efea53e96e897c1c4348933d0b44e36e91516da3
Jeri <drvonnjerryxlii@gmail.com>
2015-06-13 13:31:01 -0700

    THE INTERNET IS FINALLY EXIST! pushing updated code.

--- orange-tree.rb
+++ orange-tree.rb
@@ -1,11 +1,28 @@
 # this one is another group project! three adies to a group. :)
 # https://github.com/Ada-Developers-Academy/daily-curriculum/blob/master/topic_resources/orange_tree.md
 # -------------------------------------
-# actually, according to the new dinner requirements, I think we can move on to dessert!
 
 class OrangeTree
+  attr_accessor :soil_quality
+  attr_reader :age, :grove, :height, :orange_count
 
-  attr_reader :age, :alive, :grove, :height, :orange_count
+  # trees are young until they are over 3 years old.
+  YOUNG = 3
+
+  # trees grow faster when they are young.
+  YOUNG_GROWTH = 3
+  MATURE_GROWRTH = 0.5
+
+  # trees grow oranges differently based on soil quality.
+  HEALTHY_ORANGE_GROWTH_MULTIPLIER = 3
+  AVERAGE_ORANGE_GROWTH_MULTIPLIER = 2
+  UNHEALTHY_ORANGE_GROWTH_MULTIPLIER = 1
+
+  # trees die at different ages based on soil quality.
+  HEALTHY_DEATH_AGE = 10
+  AVERAGE_DEATH_AGE = 8
+  UNHEALTHY_DEATH_AGE = 5
+
 
   def initialize
     @age = 0
@@ -15,66 +32,56 @@ class OrangeTree
   end
 
   def grow_oranges
-    if @age >= 3 # oranges don't grow on young trees!
-      if @grove.soil_quality == "unhealthy"
-        @orange_count = @age # not many if poor soil quality
-
-      elsif @grove.soil_quality == "average"
-        @orange_count = @age * 2 # a few more if average
-
-      else
-        @orange_count = @age * 3 # otherwise healthy amounts!
+    if age >= YOUNG # oranges don't grow on young trees! those trees are focusing on growing tall!
+      if soil_quality == "unhealthy"
+        growth_multiplier = UNHEALTHY_ORANGE_GROWTH_MULTIPLIER
+      elsif soil_quality == "average"
+        growth_multiplier = AVERAGE_ORANGE_GROWTH_MULTIPLIER
+      else # "healthy"
+        growth_multiplier = HEALTHY_ORANGE_GROWTH_MULTIPLIER
       end
 
+      @orange_count = age * growth_multiplier
     end
-
   end
 
   def grow_taller
-    if @age < 5
-      @height += 2
+    if age <= YOUNG
+      @height += 3
     else
-      @height += 0.1
+      @height += 0.5
     end
   end
 
   def kill_tree
     @alive = false
     @orange_count = 0
-    @age = 0 # necessary?
-    @height = 0
+    @height = 1 # maybe they fall over & become nurse logs.
     puts "This tree has died."
   end
 
   def grow_older
-    if @grove
-      if (@grove.soil_quality == "unhealthy") && (@age >= 5)
-        kill_tree
-      elsif (@grove.soil_quality == "average") && (@age >= 8)
-        kill_tree
-      end
-    end
-
-    if age >= 10
+    if (soil_quality == "unhealthy") && (age >= UNHEALTHY_DEATH_AGE)
+      kill_tree
+    elsif (soil_quality == "average") && (age >= AVERAGE_DEATH_AGE)
+      kill_tree
+    elsif age >= HEALTHY_DEATH_AGE
       kill_tree
     else
       @age += 1
     end
-
   end
 
   def one_year_passes
-    grow_older
-    if @alive == true
-      if @age < 10
-        grow_taller
-        grow_oranges
-      end
+    if alive? && (age < HEALTHY_DEATH_AGE)
+      grow_older
+      grow_taller
+      grow_oranges
     end
   end
 
   def pick_orange
-    if @orange_count != 0
+    if orange_count > 0
       @orange_count -= 1
       puts "That was a very tasty orange!"
     else
@@ -83,50 +90,73 @@ class OrangeTree
   end
 
   def plant_on(grove_name)
-    grove_name.trees.push(self)
     @grove = grove_name
+
+    grove.trees.push(self)
+    grove.check_soil_quality
   end
 
-end
+  def alive?
+    @alive == true
+  end
+end # class OrangeTree
 
 
 class OrangeGrove
-  attr_reader :total_orange_count, :trees
+  attr_reader :count_all_the_oranges, :trees
+
+  # setting the limits for soil quality.
+  HEALTHY_LIMIT = 15 # anything less than or equal to the limit.
+  AVERAGE_LIMIT = 30 # anything over the healthy limit but less than or equal to this average limit.
+  # implied unhealthy: anything over average limit trees.
 
   def initialize
     @trees = []
-    @total_orange_count
     @soil_quality = "healthy"
   end
 
-  def soil_quality
-    if @trees.length <= 5
-      @soil_quality = "healthy"
-    elsif @trees.length <= 10
-      @soil_quality = "average"
-    else
-      @soil_quality = "unhealthy"
-    end
-  end
-
   def plant_trees(number)
-    if number.to_i != 0
+    if (number.is_a? Integer) && (number > 0)
       number.times do
         @trees.push(OrangeTree.new)
       end
 
+      check_soil_quality
     else
-      puts "You can't plant #{number} trees. Please try again with a number."
+      puts "You can't plant #{ number } trees. Please try again with a number."
     end
-
   end
 
-  def one_year_passes_grove
-    @total_orange_count = 0
-    @trees.each do |tree|
+  def one_year_passes
+    @count_all_the_oranges = 0
+    trees.each do |tree|
       tree.one_year_passes
-      @total_orange_count += tree.orange_count
+      @count_all_the_oranges += tree.orange_count
     end
+
+    check_soil_quality
   end
 
-end
+  def check_soil_quality
+    living_trees = 0
+    trees.each { |tree| living_trees += 1 if tree.alive? }
+
+    if living_trees <= HEALTHY_LIMIT
+      current_quality = "healthy"
+    elsif living_trees <= AVERAGE_LIMIT
+      current_quality = "average"
+    else
+      current_quality = "unhealthy"
+    end
+
+    update_trees_soil_quality(current_quality)
+  end
+
+  def update_trees_soil_quality(new_soil_quality)
+    @soil_quality = new_soil_quality
+
+    trees.each do |tree|
+      tree.soil_quality = new_soil_quality
+    end
+  end
+end # class OrangeGrove
